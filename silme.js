5f66661f4c19bb82469aa311fd41c5f0a6a42b11
Muhammed Baki <muhammedbakicaglayan@gmail.com>
2024-12-09 10:19:10 +0300

    meyveler dizisini oluşturduk. kaç tane meyve eklemek istendğini sorduk ve meyve adeti kadar diziye meyve eklenmesini sağladık

--- silme.js
+++ silme.js
@@ -1,1 +1,21 @@
 console.log("Projeye Başladık");
+
+//Önce bir meyveler adında bir dizi oluştur
+//kaç tane meyve eklemek istediğnii kullanıcıya sor
+//meyve sayısı kadar kullanıcıya prompt ile meyve çeşidi al
+//kullanıcıya meyve silmek istermisin diye sor
+//eğer isterse hangi meyveyi silmek istediğini sor
+//silinmesini istediği neyveyi bulup silinmesini sağla
+
+let meyveler = [];
+
+
+let meyveAdeti = Number(prompt("Kaç tane meyve eklemek istersiniz?"));
+
+
+for (let i = 0; i < meyveAdeti; i++) {
+    let meyveAdi = prompt("Meyve Çeşidi Giriniz");
+    meyveler.push(meyveAdi);
+}
+
+console.log(meyveler);
